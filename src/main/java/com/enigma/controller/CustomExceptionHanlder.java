933b2ea93e270c8c4e086a8a6cb00a984aef6b81
tomirsz <tomirsz@o2.pl>
2021-11-11 10:44:09 +0100

    Remove unused method

--- src/main/java/com/enigma/controller/CustomExceptionHanlder.java
+++ src/main/java/com/enigma/controller/CustomExceptionHanlder.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,14 +15,6 @@ import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHanlder extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
-        ErrorResponse error = new ErrorResponse(ex.getMessage(), details);
-        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
-    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
